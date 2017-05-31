b383604cee6785da862a5a0498cd7e96759b83ea
Mukesh Kumar <mkushwaha15@gmail.com>
2017-05-31 12:07:53 +0530

    commited all projects

---
+++ Demo_Junit_EJB/src/test/java/com/infindia/SimpleDemoTest.java
@@ -0,0 +1,11 @@
+package com.infindia;
+
+import org.junit.Test;
+
+public class SimpleDemoTest {
+
+	@Test
+	public void testSimple(){
+		System.out.println("Hello i am tesing unit testing");
+	}
+}
